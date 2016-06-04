7843071d65aa100b07379f8e512515e503236c8d
Den Williams <dennis.williams@loke.com.au>
2016-06-04 18:48:55 +1000

    initial version using HCI service

---
+++ nodes/flic.js
@@ -0,0 +1,41 @@
+ "use strict";
+
+var createClient = require('../lib/client');
+
+module.exports = function(RED) {
+	function flic(n) {
+		RED.nodes.createNode(this,n);
+		this.host   = n.host;
+		this.port   = n.port;
+		this.topic  = n.topic;
+		this.flic   = createClient(this.host, this.port);
+
+		var node = this;
+
+		node.status({fill:"green",shape:"dot",text:"connected"});
+
+		function onClick(evt){
+			var msg = {
+				topic: this.topic||'flic' + '/' + evt.deviceId,
+				payload: evt
+			}
+			node.send(msg);
+		}
+
+		this.flic.on('online', function(){
+			node.status({fill:"green",shape:"dot",text:"connected"});
+		});
+
+		this.flic.on('offline', function(){
+			node.status({fill:"red",shape:"dot",text:"disconnected"});
+		});
+
+		this.flic.on('click', onClick);
+
+		this.on('close', function(){
+			node.flic.removeListener('click', onClick);
+			node.flic.close();
+		});
+	}
+	RED.nodes.registerType('flic', flic);
+};
